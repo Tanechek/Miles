14cad4e51e2dab2842410f52ff2b199a4e6fa9ee
Tatyana.Popovskaya <ssttaa87@yandex.ru>
2022-06-19 22:25:20 +0700

    commit head

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticket = 40_000;
+        int bonus = 20;
+        int mile = ticket / bonus;
+        System.out.println(mile);
+    }
+
+}
